2e816b11ae1594139ab1074a4ac734f23e815019
Harrison <harrison@ao554.com>
2024-03-03 02:33:08 +0000

    first commit lol

---
+++ Debian/00-base-pkgs.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# ██   ██  █████  ██████  ██████  ██     ██     ██  ██████  ███████     ██   ██ ███████ ██████  ███████ 
+# ██   ██ ██   ██ ██   ██ ██   ██ ██     ██     ██ ██    ██    ███      ██   ██ ██      ██   ██ ██      
+# ███████ ███████ ██████  ██████  ██     ██  █  ██ ██    ██   ███       ███████ █████   ██████  █████   
+# ██   ██ ██   ██ ██   ██ ██   ██ ██     ██ ███ ██ ██    ██  ███        ██   ██ ██      ██   ██ ██      
+# ██   ██ ██   ██ ██   ██ ██   ██ ██      ███ ███   ██████  ███████     ██   ██ ███████ ██   ██ ███████ 
+                                                                                                      
+# Debian Base Packages
+# Last Updated [03-03-2024]
+
+# Check if the script is running as root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Update and upgrade
+apt update && apt upgrade -y
+
+# Install base packages
+apt install -y \
+    sudo \
+    curl \
+    wget \
+    nload \
+    vnstat \
+    net-tools \
+    dnsutils \
+    build-essential \
+    gnupg \
+    locales-all \
+    iptables \
+    python3 \
+    python3-pip \
+    axel \
+    bpytop \
+    git \
+    zsh \
+    neofetch \
+    htop \
+    tmux \
+    nano \
+    unzip \
+    zip \
+    p7zip-full \
+    unrar \
+    bzip2 \
+    ncdu \ 
+
+# Clean up
+apt autoremove -y
+apt autoclean -y
+apt clean -y
+
+# Done
+echo "Base packages installed successfully"
---
+++ README.md
@@ -0,0 +1,3 @@
+This is my Server Scripting Repository for getting it all done easier
+
+Yes I know that Ansible Exists, Leave me alone
