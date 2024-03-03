26533e28fcf518f2529ba68cc56269e82069e9e1
Harrison <24486206+AO554@users.noreply.github.com>
2024-03-03 17:28:23 +0000

    Update 03-make-swapfile.sh

--- Debian/03-make-swapfile.sh
+++ Debian/03-make-swapfile.sh
@@ -31,22 +31,28 @@ dialog --title "Swapfile Size" --menu "Choose the size of the swapfile" 10 40 7 
 64 "64GB" \
 128 "128GB" 2> /tmp/swapfile_size
 
-# Get the swapfile size from the dialog
-SWAPFILE_SIZE=$(cat /tmp/swapfile_size)
+# Check the exit status of dialog
+if [ $? -eq 0 ]; then
+    # Get the swapfile size from the dialog
+    SWAPFILE_SIZE=$(cat /tmp/swapfile_size)
 
-# Create the swapfile
-fallocate -l $SWAPFILE_SIZE /swapfile
+    # Create the swapfile
+    fallocate -l $SWAPFILE_SIZE /swapfile
 
-# Set the correct permissions
-chmod 600 /swapfile
+    # Set the correct permissions
+    chmod 600 /swapfile
 
-# Set up a Linux swap area
-mkswap /swapfile
+    # Set up a Linux swap area
+    mkswap /swapfile
 
-# Enable the swapfile
-swapon /swapfile
+    # Enable the swapfile
+    swapon /swapfile
 
-# Add the swapfile to the /etc/fstab file
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
-
+    # Add the swapfile to the /etc/fstab file
+    echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+else
+    # Exit the script if cancel was pressed
+    echo "Cancel pressed, exiting."
+    exit 0
+fi
 
