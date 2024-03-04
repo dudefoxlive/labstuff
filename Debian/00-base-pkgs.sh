7edbd573b83dc23dddf96c9db99395ba118b9e5b
dudefoxlive <50186495+dudefoxlive@users.noreply.github.com>
2024-03-03 22:35:13 -0500

    Update 00-base-pkgs.sh
    Change local settings to en_US and change base packages

--- Debian/00-base-pkgs.sh
+++ Debian/00-base-pkgs.sh
@@ -16,45 +16,24 @@ if [ "$EUID" -ne 0 ]
 fi
 
 # Configure Global Locales to en_GB.UTF-8
-export LC_ALL=en_GB.UTF-8
-export LANG=en_GB.UTF-8
-export LANGUAGE=en_GB.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
 
 # Update and upgrade
 apt update && apt upgrade -y
 
 # Install base packages
 apt install -y \
-    sudo \
-    curl \
-    wget \
-    nload \
-    vnstat \
-    net-tools \
-    dnsutils \
-    build-essential \
-    fail2ban \
-    gnupg \
-    locales-all \
-    iptables \
-    python3 \
-    python3-pip \
-    axel \
-    bpytop \
-    git \
     zsh \
+    wget \
+    git \
+    curl \
     neofetch \
     htop \
-    tmux \
-    tmate \
-    nano \
-    unzip \
-    zip \
-    p7zip-full \
-    bzip2 \
-    ncdu \
-    dialog\
-    ruby
+    vim \
+    sudo \
+    tmate 
 
 # Clean up
 apt autoremove -y
